9d47433f18a98bc69cbc214efb3d603f20fef4c3
gabrielheiwa <heiwa.gabriel@gmail.com>
2022-07-17 02:43:11 -0300

    WIP: Mensseger visualization and functions

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,15 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
+import { Dashboard } from "../pages/dashboard";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import { BsFillXDiamondFill, BsStack } from "react-icons/bs";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {routesData.map(({ path, element }) => (
+          <Route path={path} element={element} />
+        ))}
       </Routes>
     </BrowserRouter>
   );
 }
+
+export const routesData = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+];
