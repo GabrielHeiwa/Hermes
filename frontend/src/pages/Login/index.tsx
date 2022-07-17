ddd5dce4d2b3ba34fa02f46019e80398e12e74af
gabrielheiwa <heiwa.gabriel@gmail.com>
2022-07-17 01:14:56 -0300

    ADD: Initial login page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,6 +1,90 @@
+import {
+  Button,
+  Form,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+import { Controller, useForm } from "react-hook-form";
+
+// TODO: Login user function
+
+interface LoginFormProps {
+  username: string;
+  password: string;
+}
+
 function Login() {
-    return <div>Login</div>;
+  // Hooks
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<LoginFormProps>();
+
+  // Functions
+  function handleOnSubmit(data: LoginFormProps) {
+    console.log(data);
+
+    return;
   }
-  
-  export default Login;
-  
+
+  return (
+    <div
+      style={{ height: "100vh" }}
+      className="w-100 d-flex align-items-center justify-content-center"
+    >
+      <div className="w-25">
+        <Form onSubmit={handleSubmit(handleOnSubmit)}>
+          <FormGroup>
+            <Label htmlFor="username">Usuário</Label>
+            <Controller
+              control={control}
+              name="username"
+              rules={{ required: "Por favor preencha este campo" }}
+              render={({ field: { onChange } }) => (
+                <Input
+                  invalid={errors.username ? true : false}
+                  onChange={onChange}
+                  type="text"
+                  id="username"
+                  name="username"
+                />
+              )}
+            />
+            <FormFeedback>{errors.username?.message}</FormFeedback>
+          </FormGroup>
+
+          <FormGroup>
+            <Label htmlFor="password">Senha</Label>
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: "Por favor preencha este campo" }}
+              render={({ field: { onChange } }) => (
+                <Input
+                  invalid={errors.password ? true : false}
+                  onChange={onChange}
+                  type="password"
+                  id="password"
+                  name="password"
+                />
+              )}
+            />
+
+            <FormFeedback>{errors.password?.message}</FormFeedback>
+          </FormGroup>
+
+          <FormGroup>
+            <Button className="w-100" color="primary" type="submit">
+              Entrar
+            </Button>
+          </FormGroup>
+        </Form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- frontend/src/services/graphql/queries/user.ts
+++ frontend/src/services/graphql/queries/user.ts
@@ -1,6 +1,5 @@
 import { gql } from "@apollo/client";
 
-
 export const GET_USER_DATA = gql`
     query getUserData($email: String = "") {
         users (where: { email: { _eq: $email }}) {
